4435a0c447d1a4756fa71603c05baa0ddece28e3
namkha <namkha1032@gmail.com>
2023-05-09 12:53:36 +0700

    update fan control 5 state

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ AIO_FEED_ID2 = "sensor-temp"
 AIO_FEED_ID3 = "sensor-light"
 
 AIO_USERNAME = "nguyennamkha"
-AIO_KEY = ""
+AIO_KEY = "aio_BrUo26TdZK5ysTfwZ1lpn2GgFGgI"
 def connected(client):
     print ("Connect successfully...")
     client.subscribe(AIO_FEED_ID1)
--- temp/readme.md
+++
@@ -1,1 +0,0 @@
-AIO_KEY = "aio_Pbtd78gnN9g4QvKTzVU8bW4eFJQA"
